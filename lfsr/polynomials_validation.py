5e8996b50f241b6662224610faef443d5fbd2009
Philéas LAMBERT <phileas.lambert@gmail.com>
2021-09-17 16:40:36 +0200

    Showing progression during analysis

--- lfsr/polynomials_validation.py
+++ lfsr/polynomials_validation.py
@@ -1,5 +1,7 @@
 from lfsr import LFSR
 from constants import polys
+from datetime import datetime
+
 polynomial_to_analysis = polys[0]
 
 class SameValue:
@@ -21,9 +23,18 @@ pos = [1]
 for i in range(2**17):
     pos.append(lfsr.next())
 
+pourcent = 0
+initial_time = saved_time = datetime.now()        
 same_values = []
 
 for i in range(len(pos)):
+    if (i/len(pos) > pourcent + 0.05):
+        pourcent += 0.05
+        saved_time = datetime.now()
+        time_diff = (saved_time - initial_time).total_seconds()
+        estimated_time_remaining = time_diff * ( 1 / pourcent - 1 )
+        print(f"{round(pourcent*100)}% done, elapsed time: {round(time_diff/60, 1)} minutes, estimated remaining time: {round(estimated_time_remaining/60, 1)} minutes")
+
     for j in range(len(pos)):
         if pos[i]==pos[j] and i != j:
             potential_same_value = SameValue(pos[i], i, j)
