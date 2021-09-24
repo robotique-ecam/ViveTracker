1bd0099c0269f1024710250baa5205cab6b23a69
Philéas LAMBERT <phileas.lambert@gmail.com>
2021-09-24 11:40:41 +0200

    black python files

--- polynomials/polynomials_validation.py
+++ polynomials/polynomials_validation.py
@@ -1,5 +1,6 @@
 import sys
-sys.path.append('../')
+
+sys.path.append("../")
 
 from lfsr.lfsr import LFSR
 from lfsr.constants import polys
@@ -7,11 +8,12 @@ from datetime import datetime
 
 polynomial_to_analysis = polys[3]
 
+
 class SameValue:
     def __init__(self, value, index1, index2) -> None:
         self.value = value
         self.indexes = [index1, index2]
-    
+
     def __eq__(self, o: object) -> bool:
         if self.value != o.value:
             return False
@@ -22,26 +24,29 @@ class SameValue:
     def __str__(self) -> str:
         return f"{self.value} at index {self.indexes[0]} and {self.indexes[1]}"
 
+
 lfsr = LFSR(polynomial_to_analysis)
 pos = [1]
 
-for i in range(2**17):
+for i in range(2 ** 17):
     pos.append(lfsr.next())
 
 pourcent = 0
-initial_time = saved_time = datetime.now()        
+initial_time = saved_time = datetime.now()
 same_values = []
 
 for i in range(len(pos)):
-    if (i/len(pos) > pourcent + 0.05):
+    if i / len(pos) > pourcent + 0.05:
         pourcent += 0.05
         saved_time = datetime.now()
         time_diff = (saved_time - initial_time).total_seconds()
-        estimated_time_remaining = time_diff * ( 1 / pourcent - 1 )
-        print(f"{round(pourcent*100)}% done, elapsed time: {round(time_diff/60, 1)} minutes, estimated remaining time: {round(estimated_time_remaining/60, 1)} minutes")
+        estimated_time_remaining = time_diff * (1 / pourcent - 1)
+        print(
+            f"{round(pourcent*100)}% done, elapsed time: {round(time_diff/60, 1)} minutes, estimated remaining time: {round(estimated_time_remaining/60, 1)} minutes"
+        )
 
     for j in range(len(pos)):
-        if pos[i]==pos[j] and i != j:
+        if pos[i] == pos[j] and i != j:
             potential_same_value = SameValue(pos[i], i, j)
             if potential_same_value not in same_values:
                 same_values.append(potential_same_value)
@@ -50,6 +55,6 @@ for i in range(len(pos)):
 
 to_print = "Same value of polynomial " + hex(polynomial_to_analysis) + ":\n"
 for i in same_values:
-    to_print += str(i) + '\n'
+    to_print += str(i) + "\n"
 
 print(to_print)
