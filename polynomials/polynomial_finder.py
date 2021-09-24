82486ca0c93299f332bd09845b773339bb573922
Philéas LAMBERT <phileas.lambert@gmail.com>
2021-09-24 17:28:04 +0200

    a bit of rework over PolynomialIdentifier class

--- polynomials/polynomial_finder.py
+++ polynomials/polynomial_finder.py
@@ -1,5 +1,4 @@
 import sys
-from typing import List
 
 sys.path.append("../")
 
@@ -10,7 +9,7 @@ from lfsr.lfsr import LFSR
 from lfsr.constants import polys, data_period, lfsr_iteration_approx, bmc_period
 
 
-class Polynomial_identifier:
+class PolynomialIdentifier:
     def __init__(self, first_word: SingleWord, second_word: SingleWord) -> None:
         self.w_1 = first_word
         self.w_2 = second_word
@@ -22,10 +21,9 @@ class Polynomial_identifier:
 
     def __iteration_estimator(self) -> int:
         diff_ts = self.__find_diff_timestamp()
-        print(f"diff_ts {diff_ts}")
         return int(diff_ts / data_period)
 
-    def __init_LFSRs(self) -> list:
+    def __init_LFSRs(self) -> list[LFSR]:
         initial_LFSR_value = (
             self.w_1.data
             if self.w_1.start_timestamp < self.w_2.start_timestamp
@@ -33,7 +31,8 @@ class Polynomial_identifier:
         )
         return [LFSR(poly, initial_LFSR_value) for poly in polys]
 
-    def search_polynomial(self):
+    def search_polynomial(self) -> list[int]:
+        found_polys = []
         lfsrs = self.__init_LFSRs()
         final_LFSR_value = (
             self.w_1.data
@@ -47,4 +46,16 @@ class Polynomial_identifier:
                 if lfsr.next() == final_LFSR_value:
                     print(f"{hex(lfsr.poly)} found")
                     print(f"cpt {lfsr.cpt_for(final_LFSR_value)}")
+                    found_polys.append(lfsr.poly)
+        return found_polys
 
+
+bmc_decoder = BMC_decoder("../data/12MHz_100ms")
+bmc_decoder.decode_whole_document()
+
+print(bmc_decoder)
+for i in range(len(bmc_decoder.indexes_0_envelope)):
+    words = bmc_decoder.get_first_and_last_word_from_beam(i)
+
+    polynomial_identifier1 = PolynomialIdentifier(words[0], words[1])
+    polynomial_identifier1.search_polynomial()
