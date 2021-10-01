dd334afd2b9466e6406a73d7271c79d1f1e5171a
Philéas LAMBERT <phileas.lambert@gmail.com>
2021-10-01 15:10:43 +0200

    Moving script into a dedicated file

--- polynomials/polynomial_finder.py
+++ polynomials/polynomial_finder.py
@@ -9,7 +9,7 @@ sys.path.append("../")
 
 import numpy as np
 
-from bmc_decoder.bmc_decoder import BMC_decoder, SingleWord
+from bmc_decoder.bmc_decoder import SingleWord
 from lfsr.lfsr import LFSR
 from lfsr.constants import polys, data_period, lfsr_iteration_approx
 
@@ -165,17 +165,3 @@ class PolynomialIdentifier:
             found_polys.append(BeamAnalyzed(self.iteration_estimation))
 
         return found_polys
-
-
-bmc_decoder = BMC_decoder("../data/12MHz_2000ms")
-bmc_decoder.decode_whole_document()
-
-synthesis = SynthetizedBeamsAnalyzed()
-
-for i in range(len(bmc_decoder.indexes_0_envelope)):
-    words = bmc_decoder.get_first_and_last_word_from_beam(i)
-    if words != None:
-        polynomial_identifier1 = PolynomialIdentifier(words[0], words[1])
-        synthesis.add_beam(polynomial_identifier1.search_polynomial())
-
-synthesis.save_synthesis("synthesis.txt")
