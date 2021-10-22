2205a68d174949d2b45282c5a313aa85a47818c2
PhileasL <phileas.lambert@gmail.com>
2021-10-22 13:19:30 +0200

    new pll module at 96MHz

--- FPGA/pll_module/pll_module.v
+++ FPGA/pll_module/pll_module.v
@@ -6,11 +6,11 @@
  * Use at your own risk.
  *
  * Given input frequency:        12.000 MHz
- * Requested output frequency:   48.000 MHz
- * Achieved output frequency:    48.000 MHz
+ * Requested output frequency:   96.000 MHz
+ * Achieved output frequency:    96.000 MHz
  */
 
-module pll_module (
+module pll_module(
 	input  clock_in,
 	output clock_out,
 	output locked
@@ -20,7 +20,7 @@ SB_PLL40_CORE #(
 		.FEEDBACK_PATH("SIMPLE"),
 		.DIVR(4'b0000),		// DIVR =  0
 		.DIVF(7'b0111111),	// DIVF = 63
-		.DIVQ(3'b100),		// DIVQ =  4
+		.DIVQ(3'b011),		// DIVQ =  3
 		.FILTER_RANGE(3'b001)	// FILTER_RANGE = 1
 	) uut (
 		.LOCK(locked),
