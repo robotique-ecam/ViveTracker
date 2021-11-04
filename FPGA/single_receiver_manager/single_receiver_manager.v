b90c91027f1954d5ef8149db049e6b2fb9354f75
PhileasL <phileas.lambert@gmail.com>
2021-11-04 16:30:01 +0100

    Updating single_receiver_manager

--- FPGA/single_receiver_manager/single_receiver_manager.v
+++ FPGA/single_receiver_manager/single_receiver_manager.v
@@ -17,6 +17,12 @@ module single_receiver_manager (
   output wire data_availible,
   output wire [16:0] decoded_data,
   output wire [23:0] timestamp_last_data,
+
+  output wire state_led,
+  output wire state_led1,
+  output wire state_led2,
+  output wire state_led3,
+  output wire state_led4
   );
 
 reg configured;
@@ -32,5 +38,23 @@ ts4231_configurator TS4231_CONFIGURATOR (
   .configured (configured)
   );
 
+bmc_decoder #(.bit_considered (17))
+  BMC_DECODER (
+    .clk_96MHz (clk_96MHz),
+    .d_in_0 (d_in_0),
+    .d_in_1 (d_in_1),
+    .e_in_0 (e_in_0),
+    .enabled (configured),
+    .system_timestamp (system_timestamp),
+    .reset (reset),
+    .decoded_data (decoded_data),
+    .data_availible (data_availible),
+    .timestamp_last_data (timestamp_last_data),
+    .state_led (state_led),
+    .state_led1 (state_led1),
+    .state_led2 (state_led2),
+    .state_led3 (state_led3),
+    .state_led4 (state_led4)
+    );
 
 endmodule // single_receiver_manager
