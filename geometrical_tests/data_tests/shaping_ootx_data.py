a670df15ec9abb69cdea5e40b1fe7426f4ee4761
Philéas LAMBERT <phileas.lambert@gmail.com>
2021-12-09 08:55:46 +0100

    OOTX Analysis (#10)
    * Just a long run ~4/5 sec of getting values from ICE40
    
    * shaping ootx_data for later python interpretaton
    
    * Shaped ootx_data
    
    * Converting txt in python
    
    * add ootx decoder, just find each polynomials if they can be found
    
    * ootx interpreter, convert bits into floats and ints for each slow data
    
    * final ootx_values
    
    * blacked

---
+++ geometrical_tests/data_tests/shaping_ootx_data.py
@@ -0,0 +1,37 @@
+file = open("ootx0.txt", "r")
+lines = file.readlines()
+file.close()
+
+print(repr(lines[0]))
+line0 = lines[0]
+shaped = ""
+for line in lines:
+    tmp = ""
+    blank_nb = 0
+    for i in line:
+        if i not in [" ", "-"]:
+            tmp += i
+            blank_nb = 0
+        elif i == "-":
+            pass
+        elif blank_nb > 2:
+            tmp += "\n"
+            break
+        else:
+            blank_nb += 1
+
+    shaped += (
+        "[[0x"
+        + tmp[:6]
+        + ", 0x"
+        + tmp[8:14]
+        + "], [0x"
+        + tmp[16:22]
+        + ", 0x"
+        + tmp[24:30]
+        + "]],\n"
+    )
+
+file = open("ootx_shaped.txt", "a")
+file.write(shaped)
+file.close()
