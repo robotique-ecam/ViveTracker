c409058d636944c6bb09ab689871108f8bfd3a85
PhileasL <phileas.lambert@gmail.com>
2022-03-30 04:45:14 +0200

    Updating receiver_top_level_sim to be able to manage all the update

--- FPGA/receivers_top_level_sim/receivers_top_level_sim.v
+++ FPGA/receivers_top_level_sim/receivers_top_level_sim.v
@@ -2,19 +2,29 @@
 
 `include "../pll_module/pll_module_sim.v"
 `include "../serial_transmitter/serial_transmitter.v"
-`include "../triad_manager/triad_manager_sim.v"
+`include "../octo_manager/octo_manager_sim.v"
 
 module receivers_top_level_sim (
   input wire clk_25MHz,
   input wire envelop_wire_0,
   input wire envelop_wire_1,
   input wire envelop_wire_2,
+  input wire envelop_wire_3,
+  input wire envelop_wire_4,
+  input wire envelop_wire_5,
+  input wire envelop_wire_6,
+  input wire envelop_wire_7,
   input wire data_wire_0,
   input wire data_wire_1,
   input wire data_wire_2,
+  input wire data_wire_3,
+  input wire data_wire_4,
+  input wire data_wire_5,
+  input wire data_wire_6,
+  input wire data_wire_7,
   output wire tx
   );
-  
+
 wire clk_96MHz;
 wire clk_12MHz;
 wire clk_72MHz;
@@ -37,17 +47,27 @@ end
 
 wire reset_parser_0;
 wire data_avl_0;
-wire [101:0] sensor_iterations_0;
+wire [271:0] sensor_iterations_0;
 
-triad_manager_sim TRIAD0 (
+octo_manager_sim OCTO0 (
   .clk_96MHz (clk_96MHz),
   .clk_72MHz (clk_72MHz),
   .envelop_wire_0 (envelop_wire_0),
   .envelop_wire_1 (envelop_wire_1),
   .envelop_wire_2 (envelop_wire_2),
+  .envelop_wire_3 (envelop_wire_3),
+  .envelop_wire_4 (envelop_wire_4),
+  .envelop_wire_5 (envelop_wire_5),
+  .envelop_wire_6 (envelop_wire_6),
+  .envelop_wire_7 (envelop_wire_7),
   .data_wire_0 (data_wire_0),
   .data_wire_1 (data_wire_1),
   .data_wire_2 (data_wire_2),
+  .data_wire_3 (data_wire_3),
+  .data_wire_4 (data_wire_4),
+  .data_wire_5 (data_wire_5),
+  .data_wire_6 (data_wire_6),
+  .data_wire_7 (data_wire_7),
   .sys_ts (sys_ts),
   .reset_parser (reset_parser_0),
   .data_avl (data_avl_0),
